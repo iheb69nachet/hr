9989f036e1a315671226376d17f3a37aabc715b5
iheb69nachet <contact.moneem@gmail.com>
2021-06-29 17:20:15 +0200

    candidate UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-
-import './App.css';
-import NavBar from './Navbar';
+import "./App.css";
+import NavBar from "./Navbar";
 function App() {
   return (
     <div className="App">
-     <NavBar/>
+      <NavBar />
+      <div className="container-fluid justify-content-center mt-5 mb-5" >
+        <div className="row ml-5 mr-5 ">
+          <div className="col-sm-6 mr-5" style={{height:"100vh",backgroundColor:"red",borderRadius:'20px'}}>
+           <h3>Civility</h3>
+          </div>
+          <div className="col-sm-5">
+            <div className="row mb-2" style={{height:"45vh",backgroundColor:"red",borderRadius:'20px'}}>
+           
+            </div>
+            <div className="row mb-2" style={{height:"45vh",backgroundColor:"red",borderRadius:'20px'}}>
+         
+            </div>
+            <div className="row justify-content-center" style={{height:'50px'}}>
+          <button className='btn btn-success'>submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
