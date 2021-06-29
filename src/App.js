d6f40ebb1636626137d91f9357c63a1db0c16e16
iheb69nachet <contact.moneem@gmail.com>
2021-06-29 17:05:55 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import NavBar from './Navbar';
 function App() {
   return (
     <div className="App">
-     
+     <NavBar/>
     </div>
   );
 }
